a2d1de091efedb24203bee35723954e981cda11b
Mary Burney <MBURNEY@tbs-sct.gc.ca>
2022-07-13 14:10:13 -0400

    hide close button, print contents

--- TBS.PrintTest.Web/wwwroot/js/print.js
+++ TBS.PrintTest.Web/wwwroot/js/print.js
@@ -14,6 +14,16 @@
             ]);
 
         });
+    },
+
+    PrintDetail: function () {
+        var print_div = document.getElementById("print-contents");
+        var print_area = window.open();
+        print_area.document.write(print_div.innerHTML);
+        print_area.document.close();
+        print_area.focus();
+        print_area.print();
+        print_area.close();
     }
 }
 
